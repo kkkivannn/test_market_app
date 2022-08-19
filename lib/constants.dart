083f3fde5bb5cbff0b37a19a5f6d2652f2146e08
kkkivannn <you@example.com>
2022-08-19 16:36:41 +0300

    Added splash screen and begin add ThemeData

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kBackgroundColor = const Color(0xffF8F8F8);
+Color kPrimaryColor = const Color(0xff010035);
+Color kScondaryColor = const Color(0xffFF6E4E);
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:test_market_app/constants.dart';
+
+ThemeData basicTheme() => ThemeData(
+      scaffoldBackgroundColor: kBackgroundColor,
+    );
