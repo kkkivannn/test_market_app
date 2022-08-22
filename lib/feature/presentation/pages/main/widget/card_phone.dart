5afeaebab4fa3f15c4dd38d9ebc7358dad7e9947
kkkivannn <you@example.com>
2022-08-22 17:58:52 +0300

    Fixed a couple of bugs

--- lib/feature/presentation/pages/main/widget/card_phone.dart
+++ lib/feature/presentation/pages/main/widget/card_phone.dart
@@ -1,7 +1,4 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
-
-// Project imports:
 import 'package:test_market_app/constants.dart';
 
 class CardPhone extends StatelessWidget {
@@ -19,71 +16,65 @@ class CardPhone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(0),
-      child: PhysicalModel(
-        color: Colors.white,
-        elevation: 0,
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: SizedBox(
-                  child: Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: Image.network(
-                      image,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+    return PhysicalModel(
+      color: Colors.white,
+      elevation: 0,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SizedBox(
+                child: Image.network(
+                  image,
+                  fit: BoxFit.cover,
                 ),
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    '\u0024${discountPrice.toString()}',
-                    style: TextStyle(
-                      fontFamily: 'MarkPro',
-                      fontWeight: FontWeight.w700,
-                      color: kPrimaryColor,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 7),
-                    child: Text(
-                      '\u0024${priceWithoutDiscount.toString()}',
-                      style: const TextStyle(
-                        fontFamily: 'MarkPro',
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xffCCCCCC),
-                        fontSize: 10,
-                        decoration: TextDecoration.lineThrough,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 15),
-                child: Text(
-                  name,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  '\u0024${discountPrice.toString()}',
                   style: TextStyle(
                     fontFamily: 'MarkPro',
                     fontWeight: FontWeight.w700,
                     color: kPrimaryColor,
                     fontSize: 16,
                   ),
-                  textAlign: TextAlign.start,
                 ),
-              )
-            ],
-          ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 7),
+                  child: Text(
+                    '\u0024${priceWithoutDiscount.toString()}',
+                    style: const TextStyle(
+                      fontFamily: 'MarkPro',
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xffCCCCCC),
+                      fontSize: 10,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontFamily: 'MarkPro',
+                  fontWeight: FontWeight.w700,
+                  color: kPrimaryColor,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            )
+          ],
         ),
       ),
     );
